f9a9c81b0956bc2d1cc87c0e63380fa6f664df06
meladak286 <meladak286@gmail.com>
2025-02-21 15:03:29 +0300

    Create skill view
    Create skill view to display each skill proficiency, languages proficiency and degrees information

--- src/views/SkillsView.vue
+++ src/views/SkillsView.vue
@@ -4,29 +4,114 @@ import skills from "@/data/Skills.json";
 </script>
 <template>
   <div class="skills-section">
-    <SkillSlider
-      class="skill"
-      v-for="skill in skills"
-      :key="skill.name"
-      :skill="skill.name"
-      :experience="skill.experience"
-    ></SkillSlider>
+    <div class="skills">
+      <SkillSlider
+        class="skill"
+        v-for="skill in skills"
+        :key="skill.name"
+        :skill="skill.name"
+        :experience="skill.experience"
+      ></SkillSlider>
+    </div>
+    <div class="languages">
+      <h2 class="sub-title">Languages</h2>
+      <div class="language">
+        <span class="language-name capitalize">arabic</span>
+        <span class="level capitalize">native</span>
+      </div>
+      <div class="language">
+        <span class="language-name capitalize">english</span>
+        <span class="level capitalize">very good</span>
+      </div>
+    </div>
+    <div class="education">
+      <h2 class="sub-title">Education</h2>
+      <div class="degree">
+        <span class="degree-source">
+          syrian technological institute of computer
+        </span>
+        <span class="degree-info"> diploma in computer science 2020 - 2022 </span>
+      </div>
+    </div>
   </div>
 </template>
 <style scoped>
+
 .skills-section {
   width: 70%;
   margin: 50px auto;
   box-shadow: 0 0 3px 0 var(--shadow-color);
   height: 500px;
   border-radius: 25px;
+  padding: 25px 0;
+  box-sizing: border-box;
   display: grid;
   grid-template-columns: repeat(2, 1fr);
-  padding: 50px 0;
-  box-sizing: border-box;
+  grid-template-rows: auto 1fr;
+  grid-template-areas: "skills skills" "languages education";
+}
+
+.skills-section .skills {
+  height: 60%;
+  grid-area: skills;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  padding: 25px 0;
 }
 
 .skills-section .skill {
   margin: auto;
+}
+
+.sub-title {
+  font-size: 42px;
+  text-transform: capitalize;
+  color: var(--primary-font-color);
+  margin: 50px 0 25px;
+}
+
+.languages {
+  grid-area: languages;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.languages .language {
+  display: flex;
+  width: 50%;
+  justify-content: space-between;
+  color: var(--primary-font-color);
+  margin: 10px 0;
+}
+
+.languages .language span {
+  width: 25%;
+  text-align: center;
+}
+
+.education {
+  grid-area: education;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: var(--primary-font-color);
+}
+
+.education .degree {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.education .degree .degree-source {
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+
+.education .degree .degree-info {
+  font-size: 20px;
 }
 </style>
