d24654447c5abeb2d91f1751bc184baa4eb3b01a
meladak286 <meladak286@gmail.com>
2025-02-21 15:05:14 +0300

    Add skills view to the main page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <script setup>
-import homeView from "@src/views/HomeView.vue";
+import homeView from "./views/HomeView.vue";
+import SkillsView from "./views/SkillsView.vue";
 </script>
 
 <template>
   <div>
     <homeView />
+    <h1 class="section-title">Skills</h1>
+    <SkillsView />
   </div>
 </template>
 
-<style></style>
+<style>
+.section-title {
+  color: var(--highlight-color);
+  font-size: 64px;
+  font-weight: bold;
+  text-align: center;
+  letter-spacing: 1px;
+  text-shadow: 0px 2px 2px var(--shadow-color);
+  -webkit-text-stroke: 1px var(--border-color);
+}
+</style>
