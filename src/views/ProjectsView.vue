520eb3566a4cdd0dcf61daaf6c023d502b6da43e
meladak286 <meladak286@gmail.com>
2025-02-23 20:58:48 +0300

    Create a view to display projects each project in a dedicated card

---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,37 @@
+<script setup>
+import ProjectCard from "@/components/ProjectCard.vue";
+import projects from "@/data/Projects.json";
+</script>
+<template>
+  <div class="projects-section">
+    <ProjectCard
+      class="project-card"
+      v-for="(project, index) in projects"
+      :key="index"
+      :image="project.image"
+      :name="project.name"
+      :description="project.description"
+      :link="project.link"
+    ></ProjectCard>
+  </div>
+</template>
+<style scoped>
+.projects-section {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 50px;
+  width: 80%;
+  margin: auto;
+  padding: 50px 0;
+}
+
+.projects-section .project-card {
+  transition: transform 0.1s 0s linear;
+}
+
+.projects-section .project-card:hover {
+  transform: scale(1.1);
+}
+</style>
