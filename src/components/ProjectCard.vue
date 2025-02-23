e2e01996aad498f5580b607d38e5a76fd94c346f
meladak286 <meladak286@gmail.com>
2025-02-23 20:56:01 +0300

    Create project card component
    Create a component to display each project info

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,69 @@
+<script setup></script>
+<template>
+  <div class="project-card">
+    <img class="project-image" :src="image" alt="" />
+    <h3 class="project-name">{{ name }}</h3>
+    <p class="project-description">{{ description }}</p>
+    <span class="more">
+      <a :href="link" class="capitalize">View More</a>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    image: String,
+    name: String,
+    description: String,
+    link: String,
+  },
+};
+</script>
+<style scoped>
+.project-card {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  width: 250px;
+  height: 300px;
+  background-color: var(--primary-color);
+  box-shadow: 1px 1px 2px 0 var(--shadow-color);
+  border: 1px solid var(--border-color);
+  border-radius: 10px;
+  box-sizing: border-box;
+}
+
+.project-card .project-image {
+  height: 50%;
+  width: 100%;
+  border-radius: 10px 10px 0 0;
+}
+
+.project-card .project-name {
+  font-size: 24px;
+  font-weight: bold;
+  margin: 0;
+  color: var(--highlight-color);
+  padding: 0 25px;
+  overflow: hidden;
+}
+
+.project-card .project-description {
+  font-size: 12px;
+  color: var(--primary-font-color);
+  margin: 0 25px;
+  overflow: hidden;
+  width: calc(100% - 50px);
+  box-sizing: border-box;
+}
+
+.project-card .more {
+  color: var(--primary-font-color);
+  font-weight: bold;
+  font-size: 16px;
+  cursor: pointer;
+  margin: 10px 0;
+}
+</style>
