71883cc566f64633e42f8b2422a2a12c74ab4e16
meladak286 <meladak286@gmail.com>
2025-03-05 20:04:33 +0300

    Create page footer component

---
+++ src/components/PageFooter.vue
@@ -0,0 +1,68 @@
+<script setup></script>
+<template>
+  <footer class="footer">
+    <h2 class="capitalize">contact info</h2>
+    <ul class="social-links">
+      <li>
+        <a href="" class="social-link capitalize">
+          <i class="fa-solid fa-envelope"></i>
+        </a>
+      </li>
+      <li>
+        <a href="" class="social-link capitalize">
+          <i class="fa-brands fa-linkedin"></i>
+        </a>
+      </li>
+      <li>
+        <a href="" class="social-link capitalize">
+          <i class="fa-brands fa-github"></i>
+        </a>
+      </li>
+    </ul>
+  </footer>
+</template>
+<style scoped>
+.footer {
+  width: 100%;
+  padding: 25px;
+  background-color: var(--primary-color);
+  color: var(--primary-font-color);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-shadow: -2px 0 4px 0 var(--shadow-color);
+}
+
+.footer .social-links {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.footer .social-links li {
+  margin-bottom: 15px;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  font-size: 30px;
+}
+
+.footer .social-links li a {
+  color: var(--primary-font-color);
+  text-decoration: none;
+  display: inline-block;
+  text-align: center;
+  margin: 0 25px;
+  transition: transform .1s 0s;
+}
+
+.footer .social-links li a:hover{
+  transform: scale(1.5);
+}
+
+</style>
