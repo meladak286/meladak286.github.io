ac1a60f013b288148cf7d6a1980f67547ff2c72e
meladak286 <meladak286@gmail.com>
2025-03-30 11:26:56 +0300

    Add font awesome package

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
-import './assets/main.css'
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import i18n from './i18n'
+import './assets/main.css';
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import i18n from './i18n';
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core';
 
-createApp(App).use(router).use(i18n).mount('#app')
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+/* import specific icons */
+import {fas}  from '@fortawesome/free-solid-svg-icons';
+
+/* add icons to the library */
+library.add(fas);
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(router).use(i18n).mount('#app');
